970f82b42fb45cc07086d9247e54389aad762f0a
Tom Rothe <info@tomrothe.de>
2020-01-04 20:32:08 +0100

    add pins templates

--- application.lua
+++ application.lua
@@ -2,30 +2,53 @@ dofile("lib/router.lua")
 dofile("lib/server.lua")
 dofile("lib/template.lua")
 
+PINS = {
+  PIN1 = 1,
+  PIN2 = 2
+}
+
+function state_str(pin)
+  if gpio.read(pin) == 0 then
+    return "ON"
+  else
+    return "OFF"
+  end
+end
+
 function handleIndex(params)
   local template = Template:new(nil, "index.html", "layout.html")
   local vars = {}
-  vars["some_var"] = "SOME VAR"
+
+  for pinName, pinNo in pairs(PINS) do -- retrieve status of the pins
+    vars[pinName .. "_state"] = state_str(pinNo)
+  end
   
   return "200 OK", nil, template:render(vars)
 end
 
-function handleOn(params)
-  gpio.mode(0, gpio.OUTPUT)
-  gpio.write(0, gpio.LOW)
+function handleSwitch(params, pin, state)
+  gpio.write(pin, state)
   return "302 Found", "Location: /", ""
 end
 
-function handleOff(params)
-  gpio.mode(0, gpio.OUTPUT)
-  gpio.write(0, gpio.HIGH)
-  return "302 Found", "Location: /", ""
+function initGpio()
+  for _, pin in pairs(PINS) do
+    gpio.mode(pin, gpio.OUTPUT)
+    gpio.write(pin, gpio.HIGH)
+  end
 end
+
+initGpio()
 
 mainRouter = Router:new()
 mainRouter:addRoute("/", handleIndex)
-mainRouter:addRoute("/on", handleOn)
-mainRouter:addRoute("/off", handleOff)
+
+for pinName, pinNo in pairs(PINS) do -- retrieve status of the pins
+  print(pinName)
+  mainRouter:addRoute("/" .. pinName .. "/on",  function(params) return handleSwitch(params, pinNo, gpio.LOW) end)
+  mainRouter:addRoute("/" .. pinName .. "/off", function(params) return handleSwitch(params, pinNo, gpio.HIGH) end)
+end
+
 
 server = Server:new(nil, mainRouter)
 server:start()
