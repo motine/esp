1ba4648e95090b02cdbb7737c91efe7d13704a33
Tom Rothe <info@tomrothe.de>
2020-01-04 22:37:01 +0100

    remove layout feature from template (in favor of memory)

--- lib/template.lua
+++ lib/template.lua
@@ -1,22 +1,18 @@
--- If a layout is given, it shall contain `{{ _page }}` placeholder for the actual template result to be inserted
 -- Currently only simple variable substitution is supported. 
 TEMPLATE_PATH = "templates"
 
-Template = { source = nil, layout_source = nil }
+Template = { source = nil }
 
-function Template:new(o, filename, layout_filename)
+function Template:new(o, filename)
   o = o or {}
   setmetatable(o, self)
   self.__index = self
-  self.source = self:read_template(filename)
-  if layout_filename then
-    self.layout_source = self:read_template(layout_filename)
-  end
+  self:read(filename)
   return o
 end
 
 -- reads the template from the file and returns the string
-function Template:read_template(filename)
+function Template:read(filename)
   local full_path = TEMPLATE_PATH .. "/" .. filename
   if not file.exists(full_path) then
     print("Template does not exist: " .. full_path)
@@ -24,18 +20,17 @@ function Template:read_template(filename)
   end
 
   local fd = file.open(full_path, "r")
-  local template = ""
+  self.source = ""
   while true do
     local chunk = fd:read()
     if chunk == nil then break end
-    template = template .. chunk
+    self.source = self.source .. chunk
   end
   fd:close()
-  return template
 end
 
 -- renders the given source template with the given vars
-function Template:render_template(source, vars)
+function Template:render(vars)
   local subsitutionFun = function(var)
     local var_value = vars[var]
     if var_value == nil then
@@ -44,17 +39,5 @@ function Template:render_template(source, vars)
       return var_value
     end
   end
-  return source:gsub("{{%s*([%w_]+)%s*}}", subsitutionFun)
-end
-
--- renders the template and the layout (if present)
-function Template:render(vars)
-  local page = self:render_template(self.source, vars)
-
-  if self.layout_source == nil then
-    return page
-  else
-    vars["_page"] = page
-    return self:render_template(self.layout_source, vars)
-  end
+  return self.source:gsub("{{%s*([%w_]+)%s*}}", subsitutionFun)
 end
