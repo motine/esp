85dc55adbff284aa79f11d97d65260f9f5a8605c
Tom Rothe <info@tomrothe.de>
2020-01-16 18:08:58 +0100

    use json files

--- portal/README.md
+++ portal/README.md
@@ -1,3 +1,7 @@
 # MoHA
 
 For now we want to try to avoid build tools such as webpacker.
+
+```bash
+ruby -rwebrick -e's=WEBrick::HTTPServer.new(:Port => 3000, :DocumentRoot => Dir.pwd).start'
+```
--- portal/components/app.js
+++ portal/components/app.js
@@ -1,45 +1,23 @@
-
-
 const app = new Vue({
   el: "#app",
   data() {
-    // TODO put this in an actual json file
-    const DEVICE_URLS = [ "moha-lamp1.fritz.box" ]
-
     // TODO retrieve this from the device
-    const DEVICE_RESPONSES = [
-      {
-        "device": {
-          "uuid": "d54e8c2d-c84e-456b-8e5b-87455d43195f",
-          "platform": "ESP8266",
-          "comment": "built into the red box"
-        },
-        "skills": [
-          {
-            "uuid": "b82ef140-c589-49bb-81fd-7c160be2ad16",
-            "comment": "hard wired to the relay output 3",
-            "type": "switch",
-            "capabilities": ["http", "on/off"], // "brightness", "timer", "sleep-mode"
-            "actions" : {
-              "status":  { "http-path": "/moha/v1/switch1/status" },
-              "on":  { "http-path": "/moha/v1/switch1/on" },
-              "off": { "http-path": "/moha/v1/switch1/off" }
-            }
-          },
-          {
-            "uuid": "93acf110-eb0a-4334-a4bb-9ba0e305cf84",
-            "comment": "hard wired to the relay output 3",
-            "type": "switch",
-            "capabilities": ["http", "on/off"], // "brightness", "timer", "sleep-mode"
-            "actions" : {
-              "status":  { "http-path": "/moha/v1/switch1/status" },
-              "on":  { "http-path": "/moha/v1/switch1/on" },
-              "off": { "http-path": "/moha/v1/switch1/off" }
-            }
-          }
-        ]
+    return { devices: [] }
+  },
+  methods: {
+    loadFromDeviceUrls(urls) {
+      // TODO use actual urls
+      for (const url of urls) {
+        console.log("TODO use actual URL", url)
+        fetch("../tmp-response.json")
+          .then(data => data.json())
+          .then(json => this.devices.push(json));
       }
-    ]
-    return { devices: DEVICE_RESPONSES }
+    }
+  },
+  mounted() {
+    fetch("../config.json")
+      .then(data => data.json())
+      .then(json => this.loadFromDeviceUrls(json.device_urls))
   }
 })
